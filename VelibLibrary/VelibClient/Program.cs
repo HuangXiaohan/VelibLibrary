e1162dc0e6db2488fa69b1a08f53c56edad51843
HUANG XIAOHAN <xiaohan.huang@etu.unice.fr>
2018-03-14 16:30:13 +0100

    add help in application console

--- VelibLibrary/VelibClient/Program.cs
+++ VelibLibrary/VelibClient/Program.cs
@@ -12,14 +12,41 @@ namespace VelibClient
         static void Main(string[] args)
         {
             VelibServiceClient client = new VelibServiceClient();
+            string help = "This application is for searching the velib information of each city. "
+                + "\n Input the name of a city you'll get all the stations of this city"
+                + "\n Input the name of a station in this city you'll get the number of available bikes of this station";
+
+            Console.WriteLine(help);
+            while (true)
+            {
+                while(String.Compare(Console.ReadLine(), "help", true) == 0)
+                {
+                    Console.WriteLine(help);
+                }
+                Console.WriteLine("Input the city: ");
+                string city = Console.ReadLine();
+                if (client.searchStations(city) == "-1")
+                {
+                    Console.WriteLine("No such city.");
+                }
+                else
+                {
+                    Console.WriteLine("The stations of " + city + " are: " + client.searchStations(city));
+                }
+
+                Console.WriteLine("Input the station: ");
+                string station = Console.ReadLine();
+                if (client.searchBikeNum(city, station) == "-1")
+                {
+                    Console.WriteLine("No such station.");
+                }
+                else
+                {
+                    Console.WriteLine("The number of available bike is: " + client.searchBikeNum(city, station));
+                }
+                Console.ReadLine();
+            }
             
-            Console.WriteLine("Type in the city: ");
-            string city = Console.ReadLine();
-            Console.WriteLine("The stations of " + city + " are: " + client.searchStations(city));
-            Console.WriteLine("Type in the station: ");
-            string station = Console.ReadLine();
-            Console.WriteLine("The number of available bike is: "+ client.searchBikeNum(city,station));
-            Console.ReadLine();
 
         }
     }
