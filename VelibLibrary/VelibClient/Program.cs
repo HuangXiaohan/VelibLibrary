87bd76b3b853c340558beaf184a326d21c36136e
HUANG XIAOHAN <xiaohan.huang@etu.unice.fr>
2018-03-14 13:53:46 +0100

    add project

---
+++ VelibLibrary/VelibClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VelibClient.ServiceReference1;
+
+namespace VelibClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VelibServiceClient client = new VelibServiceClient();
+            
+            Console.WriteLine("Type in the city: ");
+            string city = Console.ReadLine();
+            Console.WriteLine("The stations of " + city + " are: " + client.searchStations(city));
+            Console.WriteLine("Type in the station: ");
+            string station = Console.ReadLine();
+            Console.WriteLine("The number of available bike is: "+ client.searchBikeNum(city,station));
+            Console.ReadLine();
+        }
+    }
+}
